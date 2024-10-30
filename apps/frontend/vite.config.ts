9716b69af50c0ef46f5390990a1cdca8ba5de4e0
zetharionn <zetharionn@gmail.com>
2024-10-30 15:45:15 +0300

    refactor(frontend): viteconfig

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern'
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'@app': path.resolve(__dirname, './src/app'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@widgets': path.resolve(__dirname, './src/widgets'),
+			'@features': path.resolve(__dirname, './src/features'),
+			'@entities': path.resolve(__dirname, './src/entities'),
+			'@shared': path.resolve(__dirname, './src/shared')
+		}
+	}
 })
