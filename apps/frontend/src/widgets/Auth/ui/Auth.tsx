c4a928503daa6a42db74c41a48eceb934d7c3c23
zetharionn <zetharionn@gmail.com>
2024-10-30 16:05:44 +0300

    feat(frontend): Auth

---
+++ apps/frontend/src/widgets/Auth/ui/Auth.tsx
@@ -0,0 +1,22 @@
+import { AuthForm, type AuthType } from '@features/AuthForm'
+import type { FC } from 'react'
+
+export interface IAuth {
+	type: AuthType
+}
+
+export const Auth: FC<IAuth> = ({ type }) => {
+	return (
+		<div className='flex flex-col gap-4'>
+			<div className='flex flex-col text-center'>
+				<h1 className='md:text-3xl sm:text-2xl font-medium'>
+					<span>Continue to </span>
+					<span className='font-semibold text-transparent bg-clip-text bg-gradient-to-br from-[#ffa500] to-[#8400ff]'>
+						AniTrack
+					</span>
+				</h1>
+			</div>
+			<AuthForm type={type} />
+		</div>
+	)
+}
