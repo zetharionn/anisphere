ca2dea4c9777cf402684b1f5af0c710c782de2e4
zetharionn <zetharionn@gmail.com>
2024-10-31 19:44:57 +0300

    feat(frontend): ToastService success method

--- apps/frontend/src/shared/service/ToastService/toast.service.ts
+++ apps/frontend/src/shared/service/ToastService/toast.service.ts
@@ -1,7 +1,11 @@
 import toast from 'react-hot-toast'
-import type { IToastService } from './toast.types'
+import type { IToastService, SuccessObject } from './toast.types'
 
 export class ToastService implements IToastService {
+	success(successObject?: SuccessObject) {
+		toast.success(successObject?.message ?? 'Success', successObject?.options)
+	}
+
 	error(errorObject: Error) {
 		toast.error(errorObject.message)
 	}
--- apps/frontend/src/shared/service/ToastService/toast.types.ts
+++ apps/frontend/src/shared/service/ToastService/toast.types.ts
@@ -1,3 +1,11 @@
+import type { ToastOptions } from 'react-hot-toast'
+
 export interface IToastService {
+	success: (successObject?: SuccessObject) => void
 	error: (errorObject: Error) => void
+}
+
+export interface SuccessObject {
+	message?: string
+	options?: ToastOptions
 }
