9a6b8791aaf4625faa88fb1e2c1471e31b8c78af
zetharionn <zetharionn@gmail.com>
2024-10-31 19:49:11 +0300

    refactor(frontend): ToastService error method types

--- apps/frontend/src/shared/service/ToastService/toast.service.ts
+++ apps/frontend/src/shared/service/ToastService/toast.service.ts
@@ -1,12 +1,12 @@
 import toast from 'react-hot-toast'
-import type { IToastService, SuccessObject } from './toast.types'
+import type { ErrorObject, IToastService, SuccessObject } from './toast.types'
 
 export class ToastService implements IToastService {
 	success(successObject?: SuccessObject) {
 		toast.success(successObject?.message ?? 'Success', successObject?.options)
 	}
 
-	error(errorObject: Error) {
-		toast.error(errorObject.message)
+	error(errorObject?: ErrorObject) {
+		toast.error(errorObject?.message ?? 'Error', errorObject?.options)
 	}
 }
--- apps/frontend/src/shared/service/ToastService/toast.types.ts
+++ apps/frontend/src/shared/service/ToastService/toast.types.ts
@@ -2,10 +2,14 @@ import type { ToastOptions } from 'react-hot-toast'
 
 export interface IToastService {
 	success: (successObject?: SuccessObject) => void
-	error: (errorObject: Error) => void
+	error: (errorObject?: ErrorObject) => void
 }
 
 export interface SuccessObject {
 	message?: string
+	options?: ToastOptions
+}
+
+export interface ErrorObject extends Error {
 	options?: ToastOptions
 }
