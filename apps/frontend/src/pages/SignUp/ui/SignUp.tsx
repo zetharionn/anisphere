cd5873825ece96d831c2b0ccab8a29c7b81cad1b
zetharionn <zetharionn@gmail.com>
2024-11-03 18:38:13 +0300

    chore(frontend): change formType on mount

--- apps/frontend/src/pages/SignUp/ui/SignUp.tsx
+++ apps/frontend/src/pages/SignUp/ui/SignUp.tsx
@@ -1,16 +1,23 @@
 import { useAuthStore } from '@entities/user'
+import { useAuthFormStore } from '@features/AuthForm'
 import { ROUTES } from '@shared/lib'
 import { Auth } from '@widgets/Auth'
+import { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 
 const SignUp = () => {
 	const isAuth = useAuthStore(state => state.isAuth)
+	const changeFormType = useAuthFormStore(state => state.changeFormType)
+
+	useEffect(() => {
+		changeFormType('signUp')
+	}, [changeFormType])
 
 	return (
 		<div className='flex items-center justify-center h-[85vh]'>
 			{isAuth && <Navigate to={ROUTES.HOME} />}
 			<div className='flex flex-col gap-4 lg:w-[20vw] md:w-[40vw] sm:w-[60vw]'>
-				<Auth type='signUp' />
+				<Auth />
 			</div>
 		</div>
 	)
