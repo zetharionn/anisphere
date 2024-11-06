7d90901c6ca7e4b45cad8cbae8fa1df81a4d5398
zetharionn <zetharionn@gmail.com>
2024-11-07 01:34:59 +0300

    feat(aniapi): Anilibria

--- packages/aniapi/src/api/Anilibria/Anilibria.ts
+++ packages/aniapi/src/api/Anilibria/Anilibria.ts
@@ -1,3 +1,23 @@
 import type { IAnilibria } from './Anilibria.types'
+import { Anime, Latest, Random, Search } from './methods'
+import type {
+	RandomParams,
+	AnimeParams,
+	SearchParams,
+	LatestParams
+} from './methods'
 
-export class Anilibria implements IAnilibria {}
+export class Anilibria implements IAnilibria {
+	public anime(params: AnimeParams) {
+		return Anime(params)
+	}
+	public random(params?: RandomParams) {
+		return Random(params)
+	}
+	public search(params: SearchParams) {
+		return Search(params)
+	}
+	public latest(params?: LatestParams) {
+		return Latest(params)
+	}
+}
--- packages/aniapi/src/api/Anilibria/Anilibria.types.ts
+++ packages/aniapi/src/api/Anilibria/Anilibria.types.ts
@@ -1,1 +1,14 @@
-export interface IAnilibria {}
+import type {
+	AnimeParams,
+	LatestParams,
+	RandomParams,
+	SearchParams
+} from './methods'
+import type { Release } from './types'
+
+export interface IAnilibria {
+	anime: (params: AnimeParams) => Promise<Release>
+	random: (params: RandomParams) => Promise<Release[]>
+	search: (params: SearchParams) => Promise<Release[]>
+	latest: (params: LatestParams) => Promise<Release[]>
+}
