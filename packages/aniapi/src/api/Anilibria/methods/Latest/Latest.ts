390ed44bed0c772c156dcb9f9f0f0d068750725a
zetharionn <zetharionn@gmail.com>
2024-11-07 01:19:39 +0300

    fix(aniapi): optional params

--- packages/aniapi/src/api/Anilibria/methods/Latest/Latest.ts
+++ packages/aniapi/src/api/Anilibria/methods/Latest/Latest.ts
@@ -3,7 +3,7 @@ import { Endpoints } from '../../constants'
 import type { Release } from '../../types'
 import type { LatestParams } from './Latest.types'
 
-export const Latest = (params: LatestParams) => {
+export const Latest = (params?: LatestParams) => {
 	return Fetch<Release[]>(
 		Endpoints.RELEASE_LATEST + QueryParams<LatestParams>(params, 'query')
 	)
