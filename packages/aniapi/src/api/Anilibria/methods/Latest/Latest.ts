02aee673e6f72945f4a548f79b9caa8046edc63e
zetharionn <zetharionn@gmail.com>
2024-11-07 01:18:09 +0300

    feat(aniapi): Anilibria Latest

---
+++ packages/aniapi/src/api/Anilibria/methods/Latest/Latest.ts
@@ -0,0 +1,10 @@
+import { Fetch, QueryParams } from '@/helpers'
+import { Endpoints } from '../../constants'
+import type { Release } from '../../types'
+import type { LatestParams } from './Latest.types'
+
+export const Latest = (params: LatestParams) => {
+	return Fetch<Release[]>(
+		Endpoints.RELEASE_LATEST + QueryParams<LatestParams>(params, 'query')
+	)
+}
