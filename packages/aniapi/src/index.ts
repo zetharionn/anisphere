74997f6decd1c55262691679a0cc595ec8bf3160
zetharionn <zetharionn@gmail.com>
2024-11-10 03:31:42 +0300

    fix(aniapi): export

--- packages/aniapi/src/index.ts
+++ packages/aniapi/src/index.ts
@@ -1,1 +1,16 @@
-export * from './api'
+export { Anilibria } from './api'
+export type {
+	IAnilibria,
+	Age_Rating,
+	Episode,
+	Genre,
+	GenreImage,
+	Image,
+	Names,
+	Publish_Day,
+	Release,
+	Schedule,
+	Season,
+	Timing,
+	Type
+} from './api'
